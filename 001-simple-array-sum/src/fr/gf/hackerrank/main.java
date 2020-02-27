b44b5a72574978c40f53bbd6bdd0ad7c1098d7d4
Gilbert Fernandes <gilbert.fernandes@mousquetaires.com>
2020-02-27 16:08:42 +0100

    boo

---
+++ 001-simple-array-sum/src/fr/gf/hackerrank/main.java
@@ -0,0 +1,18 @@
+package fr.gf.hackerrank;
+
+public class main {
+
+  static int simpleArraySum(int[] ar) {
+    int sum = 0;
+    for (int i : ar) {
+      sum += i;
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    int[] test = new int[] {1, 2, 3};
+    System.out.println("sum = " + simpleArraySum(test));
+  }
+
+}
---
+++ 002-compare-the-triplets/src/fr/gf/hackerrank/main.java
@@ -0,0 +1,9 @@
+package fr.gf.hackerrank;
+
+public class main {
+
+  public static void main(String[] args) {
+
+  }
+
+}
