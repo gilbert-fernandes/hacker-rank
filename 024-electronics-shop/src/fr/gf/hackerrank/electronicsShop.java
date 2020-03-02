adf16bd069ac3b22a59d9fcd49c026f90bfd48e1
Gilbert Fernandes <gilbert.fernandes@mousquetaires.com>
2020-03-02 16:25:37 +0100

    set up skeleton

---
+++ 024-electronics-shop/src/fr/gf/hackerrank/electronicsShop.java
@@ -0,0 +1,12 @@
+package fr.gf.hackerrank;
+
+public class electronicsShop {
+
+  static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
