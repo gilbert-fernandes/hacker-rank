fd6a7edb20795369b75d650250a819f25f6da178
Gilbert Fernandes <gilbert.fernandes@mousquetaires.com>
2020-03-10 12:55:56 +0100

    skeleton with todo

---
+++ 025-cats-and-a-mouse/src/fr/gf/hackerrank/catsAndAMouse.java
@@ -0,0 +1,38 @@
+package fr.gf.hackerrank;
+
+/**
+ *
+ * Two cats and a mouse are at various positions on a line. You will be given their starting positions.
+ * Your task is to determine which cat will reach the mouse first, assuming the mouse doesn't move and
+ * the cats travel at equal speed. If the cats arrive at the same time, the mouse will be allowed to move
+ * and it will escape while they fight.
+ *
+ * You are given q queries in the form of x,y and z representing the respective positions for cats A and B,
+ * and for mouse C. Complete the function catAndMouse to return the appropriate answer to each query,
+ * which will be printed on a new line.
+ *
+ * If cat A catches the mouse first, print Cat A.
+ * If cat B catches the mouse first, print Cat B.
+ *
+ * If both cats reach the mouse at the same time, print Mouse C as the two cats fight and mouse escapes.
+ *
+ * For example, cat A is at position x=2 and cat B is at y=5.
+ * If mouse is at position z=4, it is 2 units from cat A and 1 unit from cat B.
+ * Cat will catch the mouse.
+ *
+ */
+public class catsAndAMouse {
+
+  static String catAndMouse(int x, int y, int z) {
+  }
+
+  public static void main(String[] args) {
+    // sample input 0
+    // 1 2 3
+    // 1 3 2
+    // expected :
+    // Cat B
+    // Mouse C
+  }
+
+}
