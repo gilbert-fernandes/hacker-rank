363d2694c9b04b10c3e825fefb783899fc08b960
Gilbert Fernandes <gilbert.fernandes@orange.fr>
2020-03-11 16:42:38 +0100

    problem skeleton

---
+++ 030-designer-pdf-viewer/src/fr/gf/hackerrank/designerPDFviewer.java
@@ -0,0 +1,12 @@
+package fr.gf.hackerrank;
+
+public class designerPDFviewer {
+
+    static int designerPdfViewer(int[] h, String word) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
