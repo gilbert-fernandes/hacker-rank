3b72dfbf65e7a1fba51581581568ed82c9c36b52
Gilbert Fernandes <gilbert.fernandes@orange.fr>
2020-03-12 10:03:51 +0100

    done

--- 031-utopian-tree/src/fr/gf/hackerank/utopianTree.java
+++ 031-utopian-tree/src/fr/gf/hackerank/utopianTree.java
@@ -2,7 +2,27 @@ package fr.gf.hackerank;
 
 public class utopianTree {
 
-    public static void main(String[] args) {
+    static int utopianTree(int n) {
+        int height = 1;
+        for (int i = 1; i < n + 1; i++) {
+            // +1 height per cycle
+            if (i % 2 == 0) {
+                height += 1;
+            } else {
+                // double size on odd cycles
+                height *= 2;
+            }
+        }
+        return height;
+    }
 
+    public static void main(String[] args) {
+        // sample input
+        // 0, 1, 4
+        // output
+        // 1, 2, 7
+        System.out.println(utopianTree(0));
+        System.out.println(utopianTree(1));
+        System.out.println(utopianTree(4));
     }
 }
